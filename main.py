3b2b9ad2428ecc00bec74381e9b1a690c53337e3
Luka Thelen <32140456+technogeek157@users.noreply.github.com>
2017-11-06 06:31:50 -0600

    working on moving items +added classes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Created by Lucas Thelen of NGHS
 global xy, buffArr
 
 def initList(depth):#ONLY USE AT THE BEGINNING    
-    return [[0]*depth for _ in range(depth)]
+    return [['0']*depth for _ in range(depth)]
 
 def clear():
     print '\n' * 1000
@@ -19,19 +19,45 @@ def printList(l):
 def setPos(v, x, y):
     xy[y-1][x-1] = v
 
-def changePos(v, x1, y1, x2, y2):
-    if xy[y2-1][x2-1] == 0:#checks is move to space is unnocupied
-        if xy[y1-1][x1-1] == v:#checks if move from space is occupied with v
-            if x2 - x1 <= 1 or x1 - x2 <= 1:#makes sure that the distance is one or less
-                if y2 - y1 <= 1 or y1 - y2 <= 1:
-                    setPos(v, x2, y2)#sets positions
-                    setPos(0, x1, y1)
+def changePos(v, x, y, xChange, yChange):
+    if xy[y+yChange-1][x+yChange-1] == 0:#checks is move to space is unnocupied
+        if xy[y-1][x-1] == v:#checks if move from space is occupied with v
+            if yChange == -1 or yChange == 1 or xChange == 0:
+                if xChange == -1 or xChange == 1 or xChange == 0:
+                    setPos(v, x+xChange, y+yChange)#sets positions
+                    setPos('0', x, y)
+
+class lipid(object):
+    """
+    A lipid tile on a grid.
+    """
+    
+    def __init__(self, xPos, yPos):
+        """
+        Initiates where this object is
+        """
+        self.x = xPos
+        self.y = yPos
+        
+        setPos('l', self.x, self.y)
+        
+    def move(self, xMove,yMove):
+        """
+        Moves the item according to these values
+        """
+        changePos('l', self.x, self.y, xMove, yMove)
+        
+        self.x = self.x + xMove
+        self.y = self.x + yMove
 
 xy = initList(5)
 
 clear()
 
+myLipid = lipid(3,3)
+
 printList(xy)
+
+myLipid.move(1,1)
 print '\n'
-xy[2][2-1] = 1
 printList(xy)
